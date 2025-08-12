89bb0daa32e9ca02ce94079a42369f16a96ac66c
Lukáš Hrejsemnou <lukas.hrejsemnou@gmail.com>
2025-08-12 12:25:49 +0200

    Add a simple loading spinner

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,3 +1,7 @@
 export const Loader = () => {
-  return <div data-testid="loader">Loading...</div>;
+  return (
+    <div data-testid="loader" className="flex justify-center items-center mt-8">
+      <div className="w-8 h-8 border-4 border-gray-300 border-t-fuchsia-500 rounded-full animate-spin" />
+    </div>
+  );
 };
