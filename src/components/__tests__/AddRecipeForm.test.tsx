62915d5d19acdf852a826b2c5f07ee73932e601f
Lukáš Hrejsemnou <lukas.hrejsemnou@gmail.com>
2025-08-13 11:09:36 +0200

    Add more tests

--- src/components/__tests__/AddRecipeForm.test.tsx
+++ src/components/__tests__/AddRecipeForm.test.tsx
@@ -13,6 +13,7 @@ describe('AddRecipeForm', () => {
   }));
 
   beforeEach(() => {
+    jest.clearAllMocks();
     (useAddRecipeMutation as jest.Mock).mockReturnValue([mockAddRecipe]);
   });
 
@@ -86,5 +87,139 @@ describe('AddRecipeForm', () => {
 
       expect(mockUnwrap).toHaveBeenCalled();
     });
+  });
+
+  it('does not submit empty ingredients', async () => {
+    render(<AddRecipeForm />);
+
+    fireEvent.change(screen.getByPlaceholderText('Název receptu'), {
+      target: { value: 'Pizza' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Úvodní text'), {
+      target: { value: 'Cheesy' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Postup'), {
+      target: { value: 'Put in the oven' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Vaše ingredience'), {
+      target: { value: 'Cheese' },
+    });
+    fireEvent.click(screen.getByTestId('add-ingredient-button'));
+    fireEvent.change(screen.getByPlaceholderText('Čas'), {
+      target: { value: 30 },
+    });
+
+    fireEvent.click(screen.getByTestId('submit-button'));
+
+    await waitFor(() => {
+      expect(mockAddRecipe).toHaveBeenCalledWith({
+        name: 'Pizza',
+        info: 'Cheesy',
+        description: 'Put in the oven',
+        ingredients: ['Cheese'],
+        duration: 30,
+      });
+
+      expect(mockUnwrap).toHaveBeenCalled();
+    });
+  });
+
+  it('does not submit the form if nan value is in duration', async () => {
+    render(<AddRecipeForm />);
+
+    fireEvent.change(screen.getByPlaceholderText('Název receptu'), {
+      target: { value: 'Pizza' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Úvodní text'), {
+      target: { value: 'Cheesy' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Postup'), {
+      target: { value: 'Put in the oven' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Vaše ingredience'), {
+      target: { value: 'Cheese' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Čas'), {
+      target: { value: 'not a number' },
+    });
+
+    fireEvent.click(screen.getByTestId('submit-button'));
+
+    await waitFor(() => {
+      expect(screen.getByText('Čas musí mít číselnou hodnotu.')).toBeInTheDocument();
+    });
+    expect(mockAddRecipe).not.toHaveBeenCalled();
+  });
+
+  it('does not submit the form if name is missing', async () => {
+    render(<AddRecipeForm />);
+
+    fireEvent.change(screen.getByPlaceholderText('Úvodní text'), {
+      target: { value: 'Cheesy' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Postup'), {
+      target: { value: 'Put in the oven' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Vaše ingredience'), {
+      target: { value: 'Cheese' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Čas'), {
+      target: { value: 30 },
+    });
+
+    fireEvent.click(screen.getByTestId('submit-button'));
+
+    await waitFor(() => {
+      expect(screen.getByText('Recept nemůže být bez názvu.')).toBeInTheDocument();
+    });
+    expect(mockAddRecipe).not.toHaveBeenCalled();
+  });
+
+  it('does not submit the form if description is missing', async () => {
+    render(<AddRecipeForm />);
+
+    fireEvent.change(screen.getByPlaceholderText('Název receptu'), {
+      target: { value: 'Pizza' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Úvodní text'), {
+      target: { value: 'Cheesy' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Vaše ingredience'), {
+      target: { value: 'Cheese' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Čas'), {
+      target: { value: 30 },
+    });
+
+    fireEvent.click(screen.getByTestId('submit-button'));
+
+    await waitFor(() => {
+      expect(screen.getByText('Recept nemůže být bez postupu.')).toBeInTheDocument();
+    });
+    expect(mockAddRecipe).not.toHaveBeenCalled();
+  });
+
+  it('does not submit the form if there are not any ingredients', async () => {
+    render(<AddRecipeForm />);
+
+    fireEvent.change(screen.getByPlaceholderText('Název receptu'), {
+      target: { value: 'Pizza' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Úvodní text'), {
+      target: { value: 'Cheesy' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Postup'), {
+      target: { value: 'Put in the oven' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('Čas'), {
+      target: { value: 30 },
+    });
+
+    fireEvent.click(screen.getByTestId('submit-button'));
+
+    await waitFor(() => {
+      expect(screen.getByText('Musíte zadat alespoň jednu ingredienci.')).toBeInTheDocument();
+    });
+    expect(mockAddRecipe).not.toHaveBeenCalled();
   });
 });
